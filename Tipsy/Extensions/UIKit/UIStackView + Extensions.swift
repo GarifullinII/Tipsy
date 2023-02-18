c7221a8a0f78e91c57f219fcac9af3cc85059525
Ildar <ildaridrisovich@gmail.com>
2023-02-18 17:06:31 +0400

    add extension stackView and AppIcon

---
+++ Tipsy/Extensions/UIKit/UIStackView + Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIStackView + Extensions.swift
+//  Physiotherapy
+//
+//  Created by Ildar Garifullin on 13/02/2023.
+//
+
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+//let stackView = UIStackView()
+//stackView.addArrangedSubview(YourView)
+//stackView.addArrangedSubview(YourView2)
+//stackView.axis = .horizontal
+//stackView.spacing = 10
+//stackView.translatesAutoresizingMaskIntoConstraints = false
