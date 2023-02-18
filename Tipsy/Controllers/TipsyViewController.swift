be6478cb15eddb8fbfab16b34e4c6af85277cc81
Ildar <ildaridrisovich@gmail.com>
2023-02-19 00:34:47 +0400

    add views and action for buttons

--- Tipsy/Controllers/TipsyViewController.swift
+++ Tipsy/Controllers/TipsyViewController.swift
@@ -11,6 +11,8 @@ final class TipsyViewController: UIViewController {
     
     //MARK: - let/var
     
+    var buttonIsActive: Bool = false
+    
     var billTotalStackView = UIStackView()
     
     private let billTotalLabel: UILabel = {
@@ -29,7 +31,7 @@ final class TipsyViewController: UIViewController {
         textField.placeholder = "Enter bill total"
         textField.setContentHuggingPriority(UILayoutPriority(390), for: .horizontal)
         textField.backgroundColor = .clear
-        textField.font = UIFont.systemFont(ofSize: 40)
+        textField.font = UIFont.systemFont(ofSize: 35)
         textField.textAlignment = .center
         textField.textColor = .black
         textField.clearButtonMode = .always
@@ -41,10 +43,105 @@ final class TipsyViewController: UIViewController {
     
     private let calculateView: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemFill
         view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
+    }()
+    
+    var selectStackView = UIStackView()
+    
+    private let selectLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Select tip"
+        label.textColor = .systemGray
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    var buttonStackView = UIStackView()
+    
+    private let selectOneButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("0%", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        button.tintColor = .systemGreen
+        button.addTarget(self, action: #selector(selectOneButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+    
+    private let selectTenButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("10%", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        button.tintColor = .systemGreen
+        button.addTarget(self, action: #selector(selectTenButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+    
+    private let selectTwentyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("20%", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        button.tintColor = .systemGreen
+        button.addTarget(self, action: #selector(selectTwentyButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+    
+    private let chooseLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Choose Split"
+        label.textColor = .systemGray
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    var stepperStackView = UIStackView()
+    
+    private let stepperLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2"
+        label.textColor = .systemGray
+        label.font = UIFont.systemFont(ofSize: 35)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let stepper: UIStepper = {
+        let stepper = UIStepper()
+        stepper.minimumValue = 0
+        stepper.maximumValue = 100
+        stepper.stepValue = 1
+        stepper.addTarget(self, action:  #selector(stepperValueChanged), for: .valueChanged)
+        stepper.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stepper
+    }()
+    
+    private let calculateButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Calculate", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+        button.backgroundColor = .systemGreen
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(calculateButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
     }()
     
     //MARK: - life cycle funcs
@@ -59,6 +156,46 @@ final class TipsyViewController: UIViewController {
     }
     
     //MARK: - flow funcs
+    
+    @objc private func selectOneButtonTapped() {
+        changeBabackgroundColor(selectOneButton)
+        
+        buttonIsActive = !buttonIsActive
+        print("selectOneButtonTapped")
+    }
+    
+    @objc private func selectTenButtonTapped() {
+        changeBabackgroundColor(selectTenButton)
+        
+        buttonIsActive = !buttonIsActive
+        print("selectTenButtonTapped")
+    }
+    
+    @objc private func selectTwentyButtonTapped() {
+        changeBabackgroundColor(selectTwentyButton)
+        
+        buttonIsActive = !buttonIsActive
+        print("selectTwentyButtonTapped")
+    }
+    
+    @objc private func stepperValueChanged() {
+        print("stepperValueChanged")
+    }
+    
+    @objc private func calculateButtonTapped() {
+        print("calculateButtonTapped")
+    }
+    
+    private func changeBabackgroundColor(_ sender: UIButton) {
+        if buttonIsActive {
+            sender.backgroundColor = .clear
+            sender.tintColor = .systemGreen
+        } else {
+            sender.backgroundColor = .systemGreen
+            sender.tintColor = .white
+            sender.layer.cornerRadius = 10
+        }
+    }
     
     //MARK: - public
 }
@@ -77,10 +214,47 @@ extension TipsyViewController {
             spacing: 20,
             distribution: .fillProportionally
         )
-        
         view.addSubview(billTotalStackView)
         
+        buttonStackView = UIStackView(
+            arrangedSubviews: [
+                selectOneButton,
+                selectTenButton,
+                selectTwentyButton
+            ],
+            axis: .horizontal,
+            spacing: 5,
+            distribution: .fillProportionally
+        )
+        view.addSubview(buttonStackView)
+        
         view.addSubview(calculateView)
+        
+        stepperStackView = UIStackView(
+            arrangedSubviews: [
+                stepperLabel,
+                stepper
+            ],
+            axis: .horizontal,
+            spacing: 10,
+            distribution: .fillEqually
+        )
+        view.addSubview(stepperStackView)
+        
+        selectStackView = UIStackView(
+            arrangedSubviews: [
+                selectLabel,
+                buttonStackView,
+                chooseLabel,
+                stepperStackView
+            ],
+            axis: .vertical,
+            spacing: 15,
+            distribution: .fillProportionally
+        )
+        view.addSubview(selectStackView)
+        
+        calculateView.addSubview(calculateButton)
     }
 }
 
@@ -101,8 +275,19 @@ extension TipsyViewController {
             calculateView.topAnchor.constraint(equalTo: billTotalStackView.bottomAnchor, constant: 40),
             calculateView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             calculateView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-//            calculateView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9)
+        ])
+        
+        NSLayoutConstraint.activate([
+            selectStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            selectStackView.topAnchor.constraint(equalTo: billTotalStackView.bottomAnchor, constant: 60),
+            selectStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            calculateButton.centerXAnchor.constraint(equalTo: calculateView.centerXAnchor),
+            calculateButton.heightAnchor.constraint(equalToConstant: 54),
+            calculateButton.widthAnchor.constraint(equalToConstant: 200),
+            calculateButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
-
