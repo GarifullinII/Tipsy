ec5f017468124e86c6fc0ed15c015d3a2e294063
Ildar <ildaridrisovich@gmail.com>
2023-02-18 16:43:10 +0400

    Initial Commit

---
+++ Tipsy/Application/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Tipsy
+//
+//  Created by Ildar Garifullin on 18/02/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
---
+++ Tipsy/Controllers/TipsyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Ildar Garifullin on 18/02/2023.
+//
+
+import UIKit
+
+class TipsyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
